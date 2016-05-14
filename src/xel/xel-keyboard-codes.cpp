9e608bdf274cee129435a5a658adce3b0ceb0eaa
amortaza <davidnia1@gmail.com>
2016-05-13 22:16:23 -0700

    keycodes are ints

---
+++ src/xel/xel-keyboard-codes.cpp
@@ -0,0 +1,109 @@
+// binding friendly
+
+int Xel_Key_Unrecognized = 0;
+
+int Xel_Key_Back = 10;
+int Xel_Key_Tab = 11;
+int Xel_Key_Return = 12;
+
+int Xel_Key_Pause = 20;
+int Xel_Key_CapsLock = 21;
+int Xel_Key_Escape = 22;
+int Xel_Key_Space = 23;
+int Xel_Key_PageUp = 24;
+int Xel_Key_PageDown = 25;
+int Xel_Key_Home = 26;
+int Xel_Key_End = 27;
+int Xel_Key_Up = 28;
+int Xel_Key_Down = 29;
+int Xel_Key_Left = 30;
+int Xel_Key_Right = 31;
+int Xel_Key_PrintScreen = 32;
+int Xel_Key_Insert = 33;
+int Xel_Key_Delete = 34;
+
+int Xel_Key_0 = 40;
+int Xel_Key_1 = 41;
+int Xel_Key_2 = 42;
+int Xel_Key_3 = 43;
+int Xel_Key_4 = 44;
+int Xel_Key_5 = 45;
+int Xel_Key_6 = 46;
+int Xel_Key_7 = 47;
+int Xel_Key_8 = 48;
+int Xel_Key_9 = 49;
+
+int Xel_Key_A = 51;
+int Xel_Key_B = 52;
+int Xel_Key_C = 53;
+int Xel_Key_D = 54;
+int Xel_Key_E = 55;
+int Xel_Key_F = 56;
+int Xel_Key_G = 57;
+int Xel_Key_H = 58;
+int Xel_Key_I = 59;
+int Xel_Key_J = 60;
+int Xel_Key_K = 61;
+int Xel_Key_L = 62;
+int Xel_Key_M = 63;
+int Xel_Key_N = 64;
+int Xel_Key_O = 65;
+int Xel_Key_P = 66;
+int Xel_Key_Q = 67;
+int Xel_Key_R = 68;
+int Xel_Key_S = 69;
+int Xel_Key_T = 70;
+int Xel_Key_U = 71;
+int Xel_Key_V = 72;
+int Xel_Key_W = 73;
+int Xel_Key_X = 74;
+int Xel_Key_Y = 75;
+int Xel_Key_Z = 76;
+
+int Xel_Key_Num_0 = 80;
+int Xel_Key_Num_1 = 81;
+int Xel_Key_Num_2 = 82;
+int Xel_Key_Num_3 = 83;
+int Xel_Key_Num_4 = 84;
+int Xel_Key_Num_5 = 85;
+int Xel_Key_Num_6 = 86;
+int Xel_Key_Num_7 = 87;
+int Xel_Key_Num_8 = 88;
+int Xel_Key_Num_9 = 89;
+
+int Xel_Key_Multiply = 100;
+int Xel_Key_Add = 101;
+int Xel_Key_Subtract = 102;
+int Xel_Key_Decimal = 103;
+int Xel_Key_Divide = 104;
+
+int Xel_Key_F1 = 110;
+int Xel_Key_F2 = 111;
+int Xel_Key_F3 = 112;
+int Xel_Key_F4 = 113;
+int Xel_Key_F5 = 114;
+int Xel_Key_F6 = 115;
+int Xel_Key_F7 = 116;
+int Xel_Key_F8 = 117;
+int Xel_Key_F9 = 118;
+int Xel_Key_F10 = 119;
+int Xel_Key_F11 = 120;
+int Xel_Key_F12 = 121;
+
+int Xel_Key_NumLock = 130;
+int Xel_Key_ScrollLock = 131;
+
+int Xel_Key_Shift = 132;
+int Xel_Key_Control = 134;
+
+int Xel_Key_Tilde = 140;
+int Xel_Key_Left_Square = 141;
+int Xel_Key_Right_Square = 142;
+int Xel_Key_Back_Slash = 143;
+int Xel_Key_Dash = 144;
+int Xel_Key_Equal = 145;
+int Xel_Key_SemiColon = 146;
+int Xel_Key_Quote = 147;
+int Xel_Key_Comma = 148;
+int Xel_Key_Dot = 149;
+int Xel_Key_ForwardSlash = 150;
